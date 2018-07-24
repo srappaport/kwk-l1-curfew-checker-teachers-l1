ae45fcfa16d977ae509aa455e3539cfbecdec09c
srappaport <sarah.rappaport@greendot.org>
2018-07-24 13:03:41 +0000

    automatically backed up by learn

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -1,8 +1,6 @@
 def simple_curfew_checker(time)
-  if time == 11
-    puts "TROUBLE"
-  elsif time > 11
-    puts "YOURE IN TROUBLE"
+  if time >= 11
+    "You're in trouble! Better get home quick!"
   end
 end
 
